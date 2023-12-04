2d6cce6152c700dea0db372e3f306a4632e25f9c
congdat.nguyen <congdat.nguyen>
2023-12-04 10:56:02 +0700

    giao dien dang ky

--- src/app/Common/login/register/register.component.ts
+++ src/app/Common/login/register/register.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {
+  NgForm,
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  @ViewChild('signUpNgForm') signUpNgForm: NgForm | any;
 
+  signUpForm: UntypedFormGroup | any;
+  showAlert: boolean = false;
+
+  constructor(
+    private _authService: AuthService,
+    private _formBuilder: UntypedFormBuilder
+  ) {}
+
+  /**
+   * On init
+   */
+  ngOnInit(): void {
+    // Create the form
+    this.signUpForm = this._formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      company: [''],
+      agreements: ['', Validators.requiredTrue],
+    });
+  }
+  signUp(){}
 }
